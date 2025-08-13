30ff0a4164009799f40797acce3246a4db9b28c5
Gaurav Bhatt <149073059+gauravbhatt123@users.noreply.github.com>
2025-08-13 23:10:18 +0530

    Update train_model.py

--- url-security-middleware/train_model.py
+++ url-security-middleware/train_model.py
@@ -10,16 +10,18 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.utils.class_weight import compute_class_weight
 from sklearn.metrics import classification_report
 
-import tensorflow as tf
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, Conv1D, MaxPooling1D, LSTM, Dense, Dropout
+
+
 from tensorflow.keras.callbacks import EarlyStopping
 
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, Conv1D, MaxPooling1D, LSTM, Dense, Dropout
 
 # 📥 Load dataset
 print("📥 Loading dataset...")
